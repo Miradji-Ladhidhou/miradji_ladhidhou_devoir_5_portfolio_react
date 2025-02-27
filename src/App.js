75aaf35c5942cc5441d679ab972e1eb601b2d9a5
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-02-27 21:15:19 +0400

    develop footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Footer />
     </div>
   );
 }
+
+
 
 export default App;
