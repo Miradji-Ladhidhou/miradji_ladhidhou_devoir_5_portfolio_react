8a77a4fc8949289a7ee9e1b093609ba77a9c6b61
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-02-28 10:54:08 +0400

    ajout des liens pro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation.js';
-import Footer from './components/Footer.js';
+import Navigation from './components/Navigation.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
